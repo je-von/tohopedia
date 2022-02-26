4f1e27c809242db59e01435dc3a8404bb196dc6a
jevon <jevon.levin@gamil.com>
2022-02-26 16:29:40 +0700

    add PWA

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,4 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+  },
+})
